c1eb60fe8fe6c2189177f40756ac61051a8250b5
Abdullah Zübeyir Yıldız <zubeyir_55@hotmail.com>
2020-06-13 05:17:40 +0300

    blade

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,52 +1,10 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Laravel Blog - Hakkımızda</title>
-  <!-- CSS only -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-
-  </head>
-  <body>
-      <!-- Default Boostrap Navbar -->
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item ">
-              <a class="nav-link" href="/">Anasayfa</a>
-            </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="/about">Hakkımızda <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/contact">İletişim</a>
-            </li>
-          </ul>
-
-        </div>
-      </nav>
-      <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1>Hakkımda</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, itaque labore corporis earum deserunt eveniet, asperiores nobis amet ipsum soluta consequuntur? Quaerat incidunt accusantium nemo maiores? Eum consequatur architecto autem.</p>
-            </div>
-        </div>
-   
+@extends('main')
+@section('content')
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Hakkımda</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, itaque labore corporis earum deserunt eveniet, asperiores nobis amet ipsum soluta consequuntur? Quaerat incidunt accusantium nemo maiores? Eum consequatur architecto autem.</p>
       </div>
-
-   
-    <!-- JS, Popper.js, and jQuery -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  </body>
-</html>
+    </div>
+@endsection
+    
